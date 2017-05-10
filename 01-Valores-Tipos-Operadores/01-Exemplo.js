1f69d5ead81f966d27e9109b96e57928d7aa1a28
diedromeliga <diedromeliga@gmail.com>
2017-05-10 18:16:05 -0300

    criando estrutura de pasta e renomeando arquivos

---
+++ 01-Valores-Tipos-Operadores/01-Exemplo.js
@@ -0,0 +1,20 @@
+/*
+    • Capítulo 1
+      Valores, Tipos e Operadores
+*/
+
+// Operadores Unários - typeof
+/*
+    Este operador(typeof) verifica qual é o tipo de dado
+    do valor que esta sendo submetido a ele para avaliar.
+*/
+console.log(typeof 4.5); //number
+console.log(typeof "x"); //string
+console.log(typeof true); //boolean
+
+//OBS: O operador menos"-" pode ser usado como operador binário e unário.
+console.log(- (10-2)); //-8
+
+// Valores Booleanos - comparações
+console.log(3 > 2); // true
+console.log(3 < 2); // false
