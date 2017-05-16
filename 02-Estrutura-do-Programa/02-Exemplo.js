081d584fecef0dd10db5c65adc147bdacf8e1c5f
diedromeliga <diedromeliga@gmail.com>
2017-05-16 17:40:47 -0300

    finalizando loops

--- 02-Estrutura-do-Programa/02-Exemplo.js
+++ 02-Estrutura-do-Programa/02-Exemplo.js
@@ -13,3 +13,52 @@ while (counter < 10) {
 }
 console.log(result);
 // 1024
+
+// Loops Do
+// do {
+//     var name = prompt("Who are you?");
+// } while (!name) {
+//     console.log(name);
+// }
+
+// Loops For
+for (var number = 0; number <= 12; number = number + 2)
+    console.log(number);
+// 0
+// 2
+// etc
+
+// Aqui temos o código que computa 2¹⁰, usando for ao invés de while:
+var result = 1;
+for (var counter = 0; counter < 10; counter = counter + 1)
+    result = result * 2;
+console.log(result);
+//1024
+
+// Saindo de um Loop
+// Este programa encontra o primeiro número que é maior ou igual a 20, e divisível por 7:
+for (var current = 20; ; current++) {
+    if (current % 7 == 0)
+        break;
+}
+console.log(current);
+// 21
+/*
+    O truque com o operador de resto % é uma maneira fácil de testar se um número
+    é divisível por outro número. Se for, o resto da divisão é zero.
+*/
+
+// Enviando um Valor com switch
+switch (prompt("Como está o tempo?")) {
+    case "chuvoso":
+        console.log("Lembre-se de trazer um guarda-chuva!");
+        break;
+    case "ensolarado":
+        console.log("Vista roupas leves!");
+    case "nublado":
+        console.log("Vá lá fora!");
+        break;
+    default:
+        console.log("Tempo desconhecido.");
+        break;
+}
