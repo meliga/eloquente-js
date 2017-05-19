be2381c3b27a1f094026ca1c1dd26cac9c6f9cd0
diedromeliga <diedromeliga@gmail.com>
2017-05-19 09:07:03 -0300

    Criando um Triângulo com Loop for

--- 02-Estrutura-do-Programa/01-Exercicio.js
+++ 02-Estrutura-do-Programa/01-Exercicio.js
@@ -5,3 +5,11 @@ while (counter < 7) {
     result = result + "#";
     console.log(result);
 }
+
+// ou teste
+
+var result = "";
+for (var counter = 0; counter < 7; counter++){
+    result = result + "#";
+    console.log(result);
+}
