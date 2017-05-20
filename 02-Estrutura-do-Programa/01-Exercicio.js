ea071f4942121942731300fc79d8fd9ff6943726
diedromeliga <diedromeliga@gmail.com>
2017-05-20 11:22:07 -0300

    Criando um Triângulo com Loop DO

--- 02-Estrutura-do-Programa/01-Exercicio.js
+++ 02-Estrutura-do-Programa/01-Exercicio.js
@@ -1,3 +1,8 @@
+/*
+ Criando um Triângulo com Loop
+ Escreva um programa que faça 7 chamadas a console.log() para retornar o seguinte triângulo.
+*/
+// Triângulo com Loop WHILE
 var result = "";
 var counter = 0;
 while (counter < 7) {
@@ -6,10 +11,19 @@ while (counter < 7) {
     console.log(result);
 }
 
-// ou teste
-
+// Triângulo com Loop FOR
 var result = "";
 for (var counter = 0; counter < 7; counter++){
     result = result + "#";
     console.log(result);
 }
+
+// Triângulo com Loop Do
+var text = ""
+var i = 0;
+do {
+    text = text + "#";
+    i++;
+    console.log(text);
+}
+while (i < 7)
