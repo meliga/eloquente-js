901867e0e0569b61e62a0ff0f69549654dd453b8
diedromeliga <diedromeliga@gmail.com>
2017-05-20 14:38:15 -0300

    finalizando FizzBuzz

--- 02-Estrutura-do-Programa/02-Exercicio.js
+++ 02-Estrutura-do-Programa/02-Exercicio.js
@@ -14,7 +14,7 @@
 */
 
 //  - FizzBuzz com Loop FOR
-for (var counter = 0; counter <= 100; counter++){
+for (counter = 0; counter <= 100; counter++)
     if (counter % 5 == 0 && counter % 3 == 0)
         console.log("FizzBuzz");
     else if (counter % 3 == 0)
@@ -23,5 +23,5 @@ for (var counter = 0; counter <= 100; counter++){
         console.log("Buzz");
     else
         console.log(counter);
-}
+
 
