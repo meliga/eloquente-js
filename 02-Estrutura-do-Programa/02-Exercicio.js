e976d1c2c99abab5ac3ca28b9ba242ef8e5b3051
diedromeliga <diedromeliga@gmail.com>
2017-05-20 13:55:28 -0300

    FizzBuzz - imprimindo FizzBuzz no lugar dos numeros que são divisiveis por 5 e 3

--- 02-Estrutura-do-Programa/02-Exercicio.js
+++ 02-Estrutura-do-Programa/02-Exercicio.js
@@ -15,9 +15,11 @@
 
 //  - FizzBuzz com Loop FOR
 for (var counter = 0; counter <= 100; counter++){
-    if (counter % 3 == 0)
+    if (counter % 5 == 0 && counter % 3 == 0)
+        console.log("FizzBuzz");
+    else if (counter % 3 == 0)
         console.log("Fizz");
-    else if (counter % 5 == 0 && counter % 3 != 0)
+    else if (counter % 5 == 0)
         console.log("Buzz");
     else
         console.log(counter);
