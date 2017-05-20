7e87fe3a2e75bd00703fe62ea8e8e319a9f791e4
diedromeliga <diedromeliga@gmail.com>
2017-05-20 13:37:38 -0300

    FizzBuzz - imprimindo Buzz no lugar dos numeros divisiveis por 5 e não por 3

--- 02-Estrutura-do-Programa/02-Exercicio.js
+++ 02-Estrutura-do-Programa/02-Exercicio.js
@@ -17,6 +17,8 @@
 for (var counter = 0; counter <= 100; counter++){
     if (counter % 3 == 0)
         console.log("Fizz");
+    else if (counter % 5 == 0 && counter % 3 != 0)
+        console.log("Buzz");
     else
         console.log(counter);
 }
